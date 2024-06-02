19127048eae411dd8433ecfce4d1c078eb82b7c0
sejunpark <sejun0601@naver.com>
2024-06-02 11:17:43 +0900

    Bellman ford

---
+++ Bellman_Ford.py
@@ -0,0 +1,29 @@
+def bellmanFord(graph, start):
+    upper = {node: float('inf') for node in graph}
+    upper[start] = 0
+
+    for i in range(len(graph)):
+        updated = False
+        for here in graph:
+            for there, cost in graph[here].items():
+                if upper[there] > upper[here] + cost:
+                    upper[there] = upper[here] + cost
+                    updated = True
+        if updated == False: break
+    
+    if updated == True:
+        upper = None
+        
+    return upper
+
+
+graph = {
+    'A': {'B': 8, 'C': 1, 'D': 2},
+    'B': {},
+    'C': {'B': 5, 'D': 2},
+    'D': {'E': 3, 'F': 5},
+    'E': {'F': 1},
+    'F': {'A': 5}
+}
+
+print(bellmanFord(graph, 'A'))
